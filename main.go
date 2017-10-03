912090e022925906345a59650db408ee3dfc6c7a
nimcha <nimcha@cloudburst.io>
2017-10-03 12:50:02 +0200

    added 1s sleep between blocks additions

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	bc := NewBlockchain()
-
+	time.Sleep(1000 * time.Millisecond)
 	bc.AddBlock("Send 1 BTC to Ivan")
+	time.Sleep(1000 * time.Millisecond)
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Time: %d\n", block.Timestamp)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
 	}
